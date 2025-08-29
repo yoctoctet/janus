c8db8ef9a7397aef275e8beb19b93d58ab447099
Amaury Rehel <amaury.rehel@deltasigma.fr>
2025-08-29 09:53:26 +0200

    feat: rewrite everything from scratch

---
+++ lib/src/lib.cpp
@@ -0,0 +1,5 @@
+// Placeholder
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ solver/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <janus/janus.hpp>
+
+int main()
+{
+    std::cout << JANUS_VERSION << std::endl;
+    return 0;
+}
